48e9df7196de2e30aa32e2b17d76eaad09a9a99b
Mr-Suwash <suwashparajuli052@gmail.com>
2024-11-09 08:55:52 +0545

    face-app

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-
-
 from flask import Flask, render_template, Response
 import cv2
 app=Flask(__name__)
 camera = cv2.VideoCapture(0)
-
-
-
 def gen_frames():  
     while True:
         success, frame = camera.read()  # read the camera frame
@@ -15,15 +10,13 @@ def gen_frames():
         else:
             detector = cv2.CascadeClassifier(r'C:\Users\DELL\Desktop\face_detection_using_flask_and_opencv\haarcascades\haarcascade_frontalface_alt.xml')
             eye_cascade = cv2.CascadeClassifier(r'C:\Users\DELL\Desktop\face_detection_using_flask_and_opencv\haarcascades\haarcascade_eye.xml')
-
             if detector.empty():
                 print("Error loading face cascade file")
             if eye_cascade.empty():
                 print("Error loading eye cascade file")
-
             faces=detector.detectMultiScale(frame,1.1,7)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-             #Draw the rectangle around each face
+            #Draw the rectangle around each face
             for (x, y, w, h) in faces:
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
                 roi_gray = gray[y:y+h, x:x+w]
@@ -31,12 +24,10 @@ def gen_frames():
                 eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
                 for (ex, ey, ew, eh) in eyes:
                     cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 @app.route('/')
 def index():
     return render_template('index.html')
